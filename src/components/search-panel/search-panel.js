783adc238e1b275375ae186643db0e9098fc213d
Alexander Ivanov <ohmadeira@gmail.com>
2022-12-31 11:49:32 +0200

    finalysed basic React project

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+import './search-panel.css';
+
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+    
+    onUpdateSearchLow = (e) => {
+        const term = e.target.value;
+        this.setState({term: term});
+        this.props.onUpdateSearchHigh(term);
+    }
+
+    render() {
+        return (
+            <input
+                type="text"
+                className="form-control search-input" 
+                placeholder="Найти сотудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearchLow}/>
+        )
+    }
+}
+
+export default SearchPanel;
